6f6a47ef817c49bd435b39719b8d5d68d2bbb7d9
anthonyhatch <ahatch1490@gmail.com>
2022-10-24 22:30:45 -0700

    adding login files

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using react_dotnet_test.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AuthDataContextConnection") ?? throw new InvalidOperationException("Connection string 'AuthDataContextConnection' not found.");
+
+builder.Services.AddDbContext<AuthDataContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AuthDataContext>();
 
 // Add services to the container.
 
@@ -23,5 +33,6 @@ app.MapControllerRoute(
     pattern: "{controller}/{action=Index}/{id?}");
 
 app.MapFallbackToFile("index.html");;
+app.UseAuthentication();;
 
 app.Run();
