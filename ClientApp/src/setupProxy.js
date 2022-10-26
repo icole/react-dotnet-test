0833131de8811e40a1d9d0597cfcc0364f15f3c3
Ian Cole <imcole@pm.me>
2022-10-25 20:39:18 -0700

    Rescaffold app w/ identity

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -2,13 +2,19 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:8275';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:35780';
 
-const context = [
+const context =  [
   "/weatherforecast",
+  "/_configuration",
+  "/.well-known",
+  "/Identity",
+  "/connect",
+  "/ApplyDatabaseMigrations",
+  "/_framework"
 ];
 
-module.exports = function (app) {
+module.exports = function(app) {
   const appProxy = createProxyMiddleware(context, {
     target: target,
     secure: false,
--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace react_dotnet_test.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
